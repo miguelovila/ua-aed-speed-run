5e922716680d95ed4eceb7167d6916238f363d80
Miguel Vila <miguelovila@ua.pt>
2022-11-02 21:03:54 +0000

    Committing ./matlab_graphs/Solution_1_Graph.m

---
+++ matlab_graphs/Solution_1_Graph.m
@@ -0,0 +1,11 @@
+% Load Values
+valores = load("solution_1_times.txt");
+n1 = valores(1:end, 1);
+t = valores(1:end, 2);
+
+% Graph
+figure(5);
+semilogy(n1, t, '.-');
+ylabel("Tempo (s)");
+xlabel("Valores de N");
+title("Solução 1");
